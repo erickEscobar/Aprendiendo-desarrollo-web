5b889c0aa7da487659272f3ae5d93c63e55aec1f
Erick Escobar <ericking8311@gmail.com>
2022-04-06 20:00:07 -0500

    node

---
+++ Curso de node.js - express y mongoDB/node/1.mi primera app con node/app.js
@@ -0,0 +1,12 @@
+const {frutas, precios} = require('./frutas');
+
+frutas.forEach(element => {
+    console.log(element);
+});
+console.log(precios);
+console.log('cambios');
+// import frutas from './frutas'
+
+// frutas.forEach(element => {
+//     console.log(element);
+// });
---
+++ Curso de node.js - express y mongoDB/node/1.mi primera app con node/frutas.js
@@ -0,0 +1,9 @@
+const frutas = ['🍎', '🍒', '🍐', '🍓'];
+const precios = [100, 200, 300];
+
+module.exports = {
+    frutas,
+    precios
+};
+
+// export const frutas = ['🍎', '🍒', '🍐'];
