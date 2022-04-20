e3191869c2e2733543322ec165b11142ed66d62c
Erick Escobar <ericking8311@gmail.com>
2022-04-20 17:12:31 -0500

    express

--- Curso de node.js - express y mongoDB/node/3.express motores de plantilla/hbs-1-shorturl/index.js
+++ Curso de node.js - express y mongoDB/node/3.express motores de plantilla/hbs-1-shorturl/index.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const { create } = require("express-handlebars"); 
+
 const app = express();
 const port = 4000;
 
-// middleware
-app.use(express.static(__dirname + '/public'));//este middleware sirve para acceder a la carpeta public
+const hbs = create({
+    extname: ".hbs",
+});
+
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
+
 
 app.get('/', (req, res) => {
-    res.redirect('pagina principal');
-})
-app.get('/h', (req, res) => {
-    res.redirect('/otor.html');
-})
+    const urls = [
+        {origin: "www.google.com/bluuweb1", shortUrl: "fjsetwd1"},
+        {origin: "www.google.com/bluuweb2", shortUrl: "fjsetwd2"},
+        {origin: "www.google.com/bluuweb3", shortUrl: "fjsetwd3"}
+    ];
+    res.render('home', {urls: urls});
+});
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// middleware
+app.use(express.static(__dirname + '/public'));//este middleware sirve para acceder a la carpeta public
 
 app.listen(port, () => console.log('servidor levantado'));
