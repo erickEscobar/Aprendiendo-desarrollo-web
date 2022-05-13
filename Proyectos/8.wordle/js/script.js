1a97c8afc2b0d81fa1f48be18b3ff012ff6636fe
Erick Escobar <ericking8311@gmail.com>
2022-05-13 17:56:05 -0500

    wordle

--- Proyectos/8.wordle/js/script.js
+++ Proyectos/8.wordle/js/script.js
@@ -3,79 +3,32 @@ import {modo} from "./dark.js";
 const body = document.querySelector('body');
 const luz = document.getElementById('iconoLuz');
 const casilla = document.querySelectorAll('table tbody tr');
-// const borrar = document.querySelector('button#borrar img');
+const teclado = document.querySelectorAll('.teclado button');
 
 const diccionario = ['sabio', 'pagar', 'nacer', 'hadas', 'cacao', 'abeja', 'avena', 'dicta', 'cuida', 'corta', 'manga', 'aguja', 'manta', 'droga', 'poeta', 'chica', 'deuda', 'broma', 'regla', 'tarea', 'norma'];
 
-// console.log(borrar)
-const palabra = diccionario[Math.floor((Math.random() * (21 - 0 + 1)) + 0)];
-const palabraArray = palabra.split('');
-// let palabraAux = palabra;
+let palabra = diccionario[Math.floor((Math.random() * (21 - 0 + 1)) + 0)];
+let palabraArray = palabra.split('');
+// const palabraAux = palabra;
+
 let i=0, j=0;
 
 document.addEventListener('click', e => {
-    // console.log(e.target)
     if(e.target == luz){
         modo(body);
     }
+
     if(e.target.matches('.teclado button')){
         if(e.target.id != 'enviar' && e.target.id != 'borrar'){//Si tecleas en una letra
-            if(j != 5){
-                casilla[i].cells[j].innerHTML = e.target.id;
-                j++;
-                console.log(j)
-            }
+            teclearLetra(e.target.id);
         }
         if(e.target.id == 'borrar' && j != 0){//Si tecleas borrar
-            j--;
-            casilla[i].cells[j].innerHTML = "";
+            teclearBorrar();
         }
         if(e.target.id == 'enviar' && j == 5){//Si tecleas enviar
-            let letraBuscada = "";
-            for(let aux = 0; aux < 5; aux++){
-                letraBuscada += casilla[i].cells[aux].innerHTML;
-            }
-
-            if(letraBuscada == palabra) {//Si la palabra es igual
-                for(let aux = 0; aux < 5; aux++){
-                    casilla[i].cells[aux].classList.add('colorCorrect');
-                }
-                setTimeout(limpiar, 1200);
-                setTimeout(ganastes, 800);
-                
-
-                // Se reinicia
-                return i=0,j=0;
-
-            }else{
-                for(let aux = 0; aux < 5; aux++){
-    
-                    if(palabraArray[aux] != casilla[i].cells[aux].innerHTML){
-                        casilla[i].cells[aux].classList.add('colorError');
-                    }
-                    // let cuenta = 0;
-                    // let posicion = palabraAux.indexOf(casilla[i].cells[aux].innerHTML);
-                    // palabraAux = palabraAux.slice(1);
-                    let posicion = palabra.indexOf(casilla[i].cells[aux].innerHTML);
-
-                    if(posicion != -1){
-                        if(casilla[i].cells[aux].innerHTML == palabraArray[aux]){
-                            casilla[i].cells[aux].classList.add('colorCorrect');
-                        }
-                        
-                        else{
-                            casilla[i].cells[aux].classList.add('colorNotPosicion');
-                        }
-                    }
-                }
-            }
-
-            console.log(letraBuscada);
-
-            // Inicia en otra fila
-            i++;
-            j = 0;
+            teclearEnviar();
         }
+        // Si perdistes
         if(e.target.id == 'enviar' && i == 6){
             setTimeout(limpiar, 1200);
             setTimeout(perdistes, 800);
@@ -86,8 +39,35 @@ document.addEventListener('click', e => {
     }
 });
 
+document.addEventListener('keydown', e => {
+    // console.log("keyValue: " + e.key);
+    let isLetra = validarCaracter(e.key.toLowerCase());
+    // console.log(isLetra == true ? 'Es letra': 'No es letra');
+
+    if(isLetra){//Si es una letra del abecedario
+        // Si tecleas una letra
+        if(j != 5){
+            teclearLetra(e.key.toLowerCase());
+        }
+    }
+    if(e.key == 'Backspace'){//Si tecleas borrar
+        if(j != 0){//Si hay por lo menos un caracter
+            teclearBorrar();
+        }
+    }
+    if(e.key == 'Enter' && j == 5){//Si tecleas enviar
+        teclearEnviar();
+        if(i==6){
+            setTimeout(limpiar, 1200);
+            setTimeout(perdistes, 800);
+
+            // Se reinicia
+            return i=0,j=0;
+        }
+    }
 
 
+})
 
 // funcion limpiar pantalla
 function limpiar(){
@@ -99,7 +79,89 @@ function limpiar(){
             casilla[a].cells[b].classList.remove('colorNotPosicion');
         }
     }
+    teclado.forEach(tecla => {
+        tecla.classList.remove('colorCorrect');
+        tecla.classList.remove('colorError');
+        tecla.classList.remove('colorNotPosicion');
+    });
+
 }
+
+function teclearLetra(letra){
+    if(j != 5){
+        casilla[i].cells[j].innerHTML = letra;
+        j++;
+        // console.log(j)
+    }
+}
+function teclearBorrar(){
+    j--;
+    casilla[i].cells[j].innerHTML = "";
+}
+function teclearEnviar(){
+    let letraBuscada = "";
+    for(let aux = 0; aux < 5; aux++){
+        letraBuscada += casilla[i].cells[aux].innerHTML;
+    }
+
+    if(letraBuscada == palabra) {//Si la palabra es igual
+        for(let aux = 0; aux < 5; aux++){
+            casilla[i].cells[aux].classList.add('colorCorrect');
+            
+            let pintarLetra = buscarLetraTeclado(casilla[i].cells[aux].innerHTML);
+            pintarLetra.classList.add('colorCorrect');
+        }
+        setTimeout(limpiar, 1200);
+        setTimeout(ganastes, 800);
+        
+
+        // Se reinicia
+        return i=0,j=0;
+
+    }else{
+        for(let aux = 0; aux < 5; aux++){
+            let pintarLetra = buscarLetraTeclado(casilla[i].cells[aux].innerHTML);
+
+            if(palabraArray[aux] != casilla[i].cells[aux].innerHTML){
+                casilla[i].cells[aux].classList.add('colorError');
+                pintarLetra.classList.add('colorError');
+            }
+            // let cuenta = 0;
+            // let posicion = palabraAux.indexOf(casilla[i].cells[aux].innerHTML);
+            // palabraAux = palabraAux.slice(1);
+
+            let posicion = palabra.indexOf(casilla[i].cells[aux].innerHTML);
+            // console.log('posicion: ' + posicion)
+
+            if(posicion != -1){
+                pintarLetra.classList.remove('colorError');
+
+                if(casilla[i].cells[aux].innerHTML == palabraArray[aux]){
+                    casilla[i].cells[aux].classList.add('colorCorrect');
+
+                    pintarLetra.classList.remove('colorNotPosicion');
+                    pintarLetra.classList.add('colorCorrect');
+                }
+                
+                else{
+                    casilla[i].cells[aux].classList.add('colorNotPosicion');
+
+                    pintarLetra.classList.remove('colorCorrect');
+                    pintarLetra.classList.add('colorNotPosicion');
+                }
+            }
+
+
+        }
+    }
+
+    // console.log(letraBuscada);
+
+    // Inicia en otra fila
+    i++;
+    j = 0;
+}
+
 function perdistes(){
     alert('Perdiste bro 😥');
     palabra = diccionario[Math.floor((Math.random() * (21 - 0 + 1)) + 0)];
@@ -107,4 +169,18 @@ function perdistes(){
 function ganastes(){
     alert('Felicidades ganastes la partida');
     palabra = diccionario[Math.floor((Math.random() * (21 - 0 + 1)) + 0)];
+}
+
+function validarCaracter(caracter){
+    if(caracter.length === 1){
+        const eRegular = new RegExp('[A-Za-zÑñÁáÉéÍíÓóÚúÜü]{1,1}$');
+        
+        let match = eRegular.exec(caracter);
+        return match !== null ? true : false
+    }
+    return false;
+}
+function buscarLetraTeclado(letra){
+    const teclado = document.querySelector(`.teclado button#${letra}`);
+    return teclado;
 }
